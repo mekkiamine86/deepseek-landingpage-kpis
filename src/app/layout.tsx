b86a371b22d7177886d5f173850c27d6a5f01740
mekkiamine86 <mekkiamine86@gmail.com>
2026-08-10 13:32:59 +0100

    add Meta Pixel code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -152,6 +152,31 @@ export default function RootLayout({
           crossOrigin="anonymous"
         />
         <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._alg)f._alg=!0;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '3845224405787370');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=3845224405787370&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+        <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(schema) }}
         />
